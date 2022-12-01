63f59cb250cc15ded8a5a5b54083c3d2e2e0996a
kundanm881 <kundanm881@gmail.com>
2022-12-01 10:20:37 +0530

    notification

--- lib/data/notification_services.dart
+++ lib/data/notification_services.dart
@@ -1,1 +1,25 @@
-class LocalNotifition {}
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotifition {
+  static LocalNotifition instance = LocalNotifition();
+  // AndroidInitializationSettings androidSettings = AndroidInitializationSettings("@mipmap/ic_launcher.png");
+  // DarwinInitializationSettings iosSettings = DarwinInitializationSettings(
+  //   requestAlertPermission: true,
+  //   requestBadgePermission: true,
+  //   requestCriticalPermission: true,
+  //   requestSoundPermission: true,
+  // );
+  InitializationSettings initializationSettings = InitializationSettings(
+    android: AndroidInitializationSettings("@mipmap/ic_launcher.png"),
+    iOS: DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestCriticalPermission: true,
+      requestSoundPermission: true,
+    ),
+  );
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+}
